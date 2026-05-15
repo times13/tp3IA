28f5d11da0a7c7572942a34cf08019fc72ba13fa
Times <alfred.times@yahoo.fr>
2026-05-15 16:25:12 -0400

    Ajout endpoint REST avec gestion Response HTTP

--- src/main/java/ht/fds/mbds/alfred/tp3restalfred/HelloResource.java
+++ src/main/java/ht/fds/mbds/alfred/tp3restalfred/HelloResource.java
@@ -2,13 +2,22 @@ package ht.fds.mbds.alfred.tp3restalfred;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Path("/personnes/{nom}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello(@PathParam("nom") String nom) {
+
+        //return Response.ok("Hello, " + nom).build();
+        return Response.serverError()
+                .entity("Erreur serveur")
+                .build();
     }
 }
