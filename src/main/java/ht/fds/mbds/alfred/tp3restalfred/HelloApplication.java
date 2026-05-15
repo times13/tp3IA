c5ae6ec27bd113461ae21ae51fb9279f8908c2ae
Times <alfred.times@yahoo.fr>
2026-05-15 16:10:04 -0400

    Creation projet REST Jakarta EE

---
+++ src/main/java/ht/fds/mbds/alfred/tp3restalfred/HelloApplication.java
@@ -0,0 +1,9 @@
+package ht.fds.mbds.alfred.tp3restalfred;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ht/fds/mbds/alfred/tp3restalfred/HelloResource.java
@@ -0,0 +1,14 @@
+package ht.fds.mbds.alfred.tp3restalfred;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
